b1bf7a08d7511db09b64c13775ecddf30b314c94
Kaifu001 <kaifu.yang@icloud.com>
2025-01-30 16:58:48 +1300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,14 +2,14 @@ function setup() {
   createCanvas(600, 400);
   background(255);
 
-  // Call the Dog CEO API to get a random dog image
+  
   fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then(data => {
-      // Get the dog image URL from the API response
+      
       const dogImageURL = data.message;
       loadImage(dogImageURL, (img) => {
-        // Once the image is loaded, display it on the canvas
+        
         image(img, 50, 50, 500, 300);
       });
     })
@@ -19,5 +19,5 @@ function setup() {
 }
 
 function draw() {
-  // Nothing to draw here, the dog image is loaded and displayed once the API is called
+  
 }
